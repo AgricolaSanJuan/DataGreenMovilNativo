36a4ce2283c182fcf3a0f448343f34e7729d4fd2
Luiggi Giussepi Moretti Dioses <programador02@ASJ.local>
2023-11-16 15:38:43 -0500

    saved changes

---
+++ app/src/main/java/com/example/datagreenmovil/ui/TallerMain/Maquinaria/CRUD/Class/CrearRegistroOperadoresFragment.java
@@ -0,0 +1,38 @@
+package com.example.datagreenmovil.ui.TallerMain.Maquinaria.CRUD.Class;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.datagreenmovil.R;
+
+public class CrearRegistroOperadoresFragment extends Fragment {
+
+    private CrearRegistroOperadoresViewModel mViewModel;
+
+    public static CrearRegistroOperadoresFragment newInstance() {
+        return new CrearRegistroOperadoresFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_taller_maquinaria_crear_registro_operadores, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CrearRegistroOperadoresViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/datagreenmovil/ui/TallerMain/Maquinaria/CRUD/Class/CrearRegistroOperadoresViewModel.java
@@ -0,0 +1,7 @@
+package com.example.datagreenmovil.ui.TallerMain.Maquinaria.CRUD.Class;
+
+import androidx.lifecycle.ViewModel;
+
+public class CrearRegistroOperadoresViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
