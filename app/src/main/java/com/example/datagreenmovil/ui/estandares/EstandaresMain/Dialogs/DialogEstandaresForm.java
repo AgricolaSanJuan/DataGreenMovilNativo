49156c084509e9d3fd0112b1e6c85f977bf23cfa
Luiggi Moretti Dioses <luiggigmd_97@outlook.com>
2025-01-17 12:02:41 -0500

    sin material design

--- app/src/main/java/com/example/datagreenmovil/ui/estandares/EstandaresMain/Dialogs/DialogEstandaresForm.java
+++ app/src/main/java/com/example/datagreenmovil/ui/estandares/EstandaresMain/Dialogs/DialogEstandaresForm.java
@@ -60,7 +60,9 @@ public class DialogEstandaresForm extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         // Usando MaterialAlertDialogBuilder
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+//        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity());
+
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         binding = FragmentEstandaresFormBinding.inflate(inflater);
         builder.setView(binding.getRoot());
@@ -156,51 +158,51 @@ public class DialogEstandaresForm extends DialogFragment {
         });
 
         // Configuración de los botones
-        if (positiveButton) {
-            builder.setPositiveButton("Guardar", (dialog, which) -> {
-                Swal.confirm(context, "GUARDAR ESTANDAR", "Está seguro de guardar este estandar?").setConfirmClickListener(sweetAlertDialog -> {
-                    estandaresDAO.insertarEstandares(estandarActual);
-                    setAction.setPositiveAction();
-                }).setCancelClickListener(sweetAlertDialog -> {
-                    sweetAlertDialog.dismissWithAnimation();
-                });
-            });
-        }
-        if (negativeButton) {
-            builder.setNegativeButton("Cancelar", (dialog, which) -> {
-                setAction.setNegativeAction();
-            });
-        }
-        if (neutralButton) {
-            builder.setNeutralButton("Cerrar", (dialog, which) -> {
-                setAction.setNeutralAction();
-            });
-        }
+//        if (positiveButton) {
+//            builder.setPositiveButton("Guardar", (dialog, which) -> {
+//                Swal.confirm(context, "GUARDAR ESTANDAR", "Está seguro de guardar este estandar?").setConfirmClickListener(sweetAlertDialog -> {
+//                    estandaresDAO.insertarEstandares(estandarActual);
+//                    setAction.setPositiveAction();
+//                }).setCancelClickListener(sweetAlertDialog -> {
+//                    sweetAlertDialog.dismissWithAnimation();
+//                });
+//            });
+//        }
+//        if (negativeButton) {
+//            builder.setNegativeButton("Cancelar", (dialog, which) -> {
+//                setAction.setNegativeAction();
+//            });
+//        }
+//        if (neutralButton) {
+//            builder.setNeutralButton("Cerrar", (dialog, which) -> {
+//                setAction.setNeutralAction();
+//            });
+//        }
 
         // Crear el diálogo
-        AlertDialog dialog = builder.create();
+        android.app.AlertDialog dialog = builder.create();
 
         // Asegúrate de que el diálogo se muestre completamente antes de modificar los botones
         dialog.setOnShowListener(dialogInterface -> {
             Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
             Button negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
-            Button neutralButton = dialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+//            Button neutralButton = dialog.getButton(AlertDialog.BUTTON_NEUTRAL);
 
             // Asegúrate de que las referencias a los botones no sean null
-            if (positiveButton != null) {
-                positiveButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
-                positiveButton.setBackgroundColor(ContextCompat.getColor(context, R.color.azulPrimario));
-            }
+//            if (positiveButton != null) {
+//                positiveButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
+//                positiveButton.setBackgroundColor(ContextCompat.getColor(context, R.color.azulPrimario));
+//            }
+//
+//            if (negativeButton != null) {
+//                negativeButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
+//                negativeButton.setBackgroundColor(ContextCompat.getColor(context, R.color.alerta));
+//            }
 
-            if (negativeButton != null) {
-                negativeButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
-                negativeButton.setBackgroundColor(ContextCompat.getColor(context, R.color.alerta));
-            }
-
-            if (neutralButton != null) {
-                neutralButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
-                neutralButton.setBackgroundColor(ContextCompat.getColor(context, R.color.grisOscuro));
-            }
+//            if (neutralButton != null) {
+//                neutralButton.setTextColor(ContextCompat.getColor(context, R.color.blancoOficial));
+//                neutralButton.setBackgroundColor(ContextCompat.getColor(context, R.color.grisOscuro));
+//            }
 
             // Modificar la altura del diálogo
             View dialogView = dialog.findViewById(android.R.id.content);
